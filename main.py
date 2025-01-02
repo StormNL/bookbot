8f8a410c9109430e858869ca794f2da871d62dbe
StormWood1 <laurens_storm@Hotmail.com>
2025-01-02 10:00:35 +0100

    B: word counter toegevoegd

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-    print(file_contents)
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    word_count = count_words(text)
+    print(word_count)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+
+
+def count_words(text):
+    text_split = text.split()
+    word_count = len(text_split)
+    return word_count
+
 main()
 
-def count_words():
-    
 
     
