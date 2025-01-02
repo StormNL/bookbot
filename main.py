14f78531d17e62afbd175ff4136666f7f73818ff
StormWood1 <laurens_storm@Hotmail.com>
2025-01-02 11:02:01 +0100

    Test

--- main.py
+++ main.py
@@ -2,20 +2,21 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     word_count = count_words(text)
-    print(word_count)
-
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-
-
-
 def count_words(text):
     text_split = text.split()
     word_count = len(text_split)
     return word_count
+
+def count_characters(text):
+    characters = []
+    lower_characters = text.lower()
+    
+    
 
 main()
 
